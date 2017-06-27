2b81a8db515e7e3263e7cf257e66e96b353e331c
Awinja-Andela <joan.awinja@andela.com>
2017-06-27 12:08:16 +0300

    added readme and screenshot

--- Bucketlist/src/app/app.module.ts
+++ Bucketlist/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule} from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
 import { routing } from './app.routing';
 import { AppConfig } from './app.config';
 import { AppComponent } from './app.component';
